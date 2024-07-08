7e211b8c9aad83453c598d4d333a06647e86038c
Tejas <tejasg263@gmail.com>
2024-07-08 20:28:50 +0530

    Bug Fix

--- babel.config.js
+++ babel.config.js
@@ -2,14 +2,6 @@ module.exports = function (api) {
   api.cache(true);
   const plugins = [];
 
-  plugins.push([
-    "@tamagui/babel-plugin",
-    {
-      components: ["tamagui"],
-      config: "./tamagui.config.ts",
-    },
-  ]);
-
   plugins.push(["@babel/plugin-proposal-decorators", { legacy: true }]);
 
   return {
